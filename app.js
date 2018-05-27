8e54c9668553e623ac6eb21015332e2ed23722b6
Lucas Fernandes <lucasfads@gmail.com>
2018-05-27 13:23:15 -0300

    v1.3 themes allowed. Credits added

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ app.get("/", function(req, res){
 	})
 })
 
-
+app.get("/g/:theme", function(req, res){
+	var theme = req.params.theme;
+	var themeJson = "https://listsdesign.herokuapp.com/lists/" + theme + ".json"
+	request(themeJson, function (error, response, body) {
+		if(!error && response.statusCode == 200) {
+			var parsedData = JSON.parse(body);
+			res.render("hangman", {parsedData: parsedData});
+		}
+	})
+})
 
 app.listen(3000, function(){
   console.log("The man will be hanged");
